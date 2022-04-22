ca64953ae3c6eb35b764a555b1482701452cf486
TominoFTW <99618066+TominoFTW@users.noreply.github.com>
2022-04-23 01:24:24 +0200

    Doxygen fix

--- src/MathLib.py
+++ src/MathLib.py
@@ -4,18 +4,15 @@
 ##
 # @package MathLib
 # Mathematical library for calculator.
-# Consists of mathematical operations.
-# 
 
 ##
-# @brief Class for library
-#
+# @brief Class for library of mathematical operations.
 class MathLib:
+
     ##
     # @brief      Adds two numbers
-    #
-    # @param      a 
-    # @param      b 
+    # @param      a addend
+    # @param      b addend
     # @return     The sum of a and b
     @staticmethod
     def add(a, b):
@@ -23,9 +20,8 @@ class MathLib:
 
     ##
     # @brief      Subtracts two numbers
-    #
-    # @param      a
-    # @param      b
+    # @param      a minuend
+    # @param      b subtrahend
     # @return     The difference of a and b
     @staticmethod
     def sub(a, b):
@@ -33,19 +29,17 @@ class MathLib:
 
     ##
     # @brief      Multiplies two numbers
-    #
-    # @param      a
-    # @param      b
+    # @param      a multiplicand
+    # @param      b multiplier
     # @return     The product of a and b
     @staticmethod
     def mul(a, b):
-        return a*b
+        return a * b
 
     ##
     # @brief      Divides two numbers
-    #
-    # @param      a
-    # @param      b
+    # @param      a dividend
+    # @param      b divisor
     # @exception  ValueError If b is 0
     # @return     The quotient of a and b
     @staticmethod
@@ -53,12 +47,11 @@ class MathLib:
         if (b == 0):
             raise ValueError("MathError")
         if a % b == 0:
-            return a/b
-        return float(a)/b
+            return a / b
+        return float(a) / b
 
     ##
     # @brief      Calculates the factorial of a number  
-    #
     # @param      n number to calculate factorial of
     # @exception  ValueError If the number is negative nor integer
     # @return     factorial of the number
@@ -73,7 +66,6 @@ class MathLib:
 
     ##
     # @brief      Calculates the power of a number
-    #
     # @param      base the base of the power
     # @param      exponent the exponent of the power
     # @exception  ValueError If the exponent is negative nor integer
@@ -87,7 +79,6 @@ class MathLib:
 
     ##
     # @brief      Calculates the root of a number
-    #
     # @param      base the base of the root
     # @param      root the value of the root
     # @exception  ValueError if the base is negative
@@ -101,7 +92,6 @@ class MathLib:
 
     ##
     # @brief      Calculates the absolute value of a number
-    #
     # @param      n number to calculate the absolute value of
     # @return     absolute value of the number
     @staticmethod
