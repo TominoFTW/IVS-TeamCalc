b2deb1f414dcee65fd79b774b46ddb67bf1d37b5
TominoFTW <99618066+TominoFTW@users.noreply.github.com>
2022-04-28 21:17:59 +0200

    Fix in profiling.py

--- src/profiling.py
+++ src/profiling.py
@@ -7,36 +7,40 @@ import random
 
 
 def standard_deviation(nums):
-    s = MathLib.root(MathLib.div(suma(nums),MathLib.sub(len(nums),1)),2)
+    s = MathLib.root(MathLib.div(suma(nums), MathLib.sub(len(nums), 1)), 2)
     return s
 
-def input_data(input):
-    input = input.replace("\t"," ").split(" ")
+
+def input_data(numbers, input):
+    input = input.replace("\t", " ").split(" ")
     for i in input:
         if i != "":
             numbers.append(int(i))
 
     return numbers
 
-def random_input(nums):
-    for i in range(random.randint(1,1001)): #max number of input numbers
-        nums.append(random.randint(-1000, 1000))
-        
-    return nums
 
-def suma(nums):
+def random_input(numbers):
+    for i in range(random.randint(1000, 1001)):  # max number of input numbers
+        numbers.append(random.randint(-1000, 1000))
+
+    return numbers
+
+
+def suma(numbers):
     sum = 0
-    for i in nums:
-        sum = MathLib.add(MathLib.power(MathLib.sub(i, prumer(nums)),2),sum)
+    for i in numbers:
+        sum = MathLib.add(MathLib.power(MathLib.sub(i, prumer(numbers)), 2), sum)
 
     return sum
 
-def prumer(nums):
+
+def prumer(numbers):
     sum = 0
-    for i in nums:
+    for i in numbers:
         sum = MathLib.add(i, sum)
 
-    return MathLib.div(sum,len(nums))
+    return MathLib.div(sum, len(numbers))
 
 
 if __name__ == '__main__':
@@ -45,7 +49,7 @@ if __name__ == '__main__':
     if string == "random":
         numbers = random_input(numbers)
     else:
-        numbers = input_data(string)
+        numbers = input_data(numbers, string)
 
     print("\nStandardni odchylka je:", standard_deviation(numbers))
 
